090f9b1cb7b0fa9415a60c843bdc30b1ee332b9d
du11jk <825174096@qq.com>
2022-04-28 23:32:59 +0800

    feat($router): 解耦部分代码，统一路由

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { info } = require('console');
 var express = require('express');
 var router = express.Router();
 
@@ -6,41 +7,71 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* get return data */
+function return_data(sample_name, step_index, api){
+  var fs =require('fs');
+  let info_file = fs.readFileSync('./public/samples/' + sample_name + '/info.json');
+  let info_data = JSON.parse(info_file);
+  let rdata = "";
+  if(info.name == "test" && info.step == 0){
+    rdata = "案例没有加载";
+  }
+  else if(step_index > info_data.step){
+    rdata = "案例已经结束";
+  }
+  else{
+    file_path = './public/samples/' + sample_name + '/step' + step_index + api + '.simudata';
+    rdata = fs.readFileSync(file_path).toString();
+  }
+  return rdata;
+}
 // ASM
 router.get('/asm', function (req, res, next) {
   sample_name = req.query.sample_name;
   step_index = req.query.step_index;
-  file_path = './public/samples/' + sample_name + '/step' + step_index + '/asm.simudata';
-  const fs =require('fs');
-  fs.readFile(file_path, (err, data) => {
-    if (err) throw err;
-    res.end(data.toString());
-  });
+  let rdata = return_data(sample_name, step_index, "/asm");
+  res.end(rdata);
 });
+
+// REG
+router.get('/reg', function (req, res, next) {
+  sample_name = req.query.sample_name;
+  step_index = req.query.step_index;
+  let rdata = return_data(sample_name, step_index, "/reg");
+  res.end(rdata);
+});
+
+// STACK
+router.get('/stack', function (req, res, next) {
+  sample_name = req.query.sample_name;
+  step_index = req.query.step_index;
+  let rdata = return_data(sample_name, step_index, "/stack");
+  res.end(rdata);
+});
+
 
 // SYMBOLS
 router.get('/symbols', function(req, res, next) {
   sample_name = req.query.sample_name;
   step_index = req.query.step_index;
-  file_path = './public/samples/' + sample_name + '/step' + step_index + '/symbols.simudata';
-  const fs =require('fs');
-  fs.readFile(file_path, (err, data) => {
-    if (err) throw err;
-    console.info(data.toString())
-    res.end(data.toString());
-  });
+  let rdata = return_data(sample_name, step_index, "/symbols");
+  res.end(rdata);
 });
 
 // MEM MAP
 router.get('/memmap', function (req, res, next) {
   sample_name = req.query.sample_name;
   step_index = req.query.step_index;
-  file_path = './public/samples/' + sample_name + '/step' + step_index + '/memmap.simudata';
-  const fs =require('fs');
-  fs.readFile(file_path, (err, data) => {
-    if (err) throw err;
-    res.end(data.toString());
-  });
+  let rdata = return_data(sample_name, step_index, "/memmap");
+  res.end(rdata);
+});
+
+// NETWORK
+router.get('/network', function (req, res, next) {
+  sample_name = req.query.sample_name;
+  step_index = req.query.step_index;
+  let rdata = return_data(sample_name, step_index, "/network");
+  res.end(rdata);
 });
 
 
